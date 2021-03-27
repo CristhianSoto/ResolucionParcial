4e32c53547b0535ec605525987e27cc69834e123
CristhianSoto <a2018210980@estudiantes.upsa.edu.bo>
2021-03-27 11:40:51 -0400

    Se aumento post al AddController

--- SRecuparacion_Cristhian/Recuparacion_Cristhian/Controllers/AddController.cs
+++ SRecuparacion_Cristhian/Recuparacion_Cristhian/Controllers/AddController.cs
@@ -10,7 +10,15 @@ namespace Recuparacion_Cristhian.Controllers
     public class AddController : ApiController
     {
         [HttpGet]
+        [HttpPost]
         public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        [Route("{a}/{b}")]
+        public int Sumar2(int a, int b)
         {
             return a + b;
         }
